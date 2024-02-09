0e2b91c2b586bb28c6f53c83b2297951e1b1e493
Jakub Augustyn <kubaugustyn@gmail.com>
2024-02-09 17:22:34 +0100

    initial commit

---
+++ lib/fcm_travel_plan.ex
@@ -0,0 +1,18 @@
+defmodule FcmTravelPlan do
+  @moduledoc """
+  Documentation for `FcmTravelPlan`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FcmTravelPlan.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fcm_travel_plan_test.exs
@@ -0,0 +1,8 @@
+defmodule FcmTravelPlanTest do
+  use ExUnit.Case
+  doctest FcmTravelPlan
+
+  test "greets the world" do
+    assert FcmTravelPlan.hello() == :world
+  end
+end
